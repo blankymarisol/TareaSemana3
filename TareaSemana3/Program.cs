80045283a1b5b2331f9bcca1a2ac8b986d5dbc5c
blanksss <marrophph@gmail.com>
2024-02-23 22:54:28 -0600

    cuarto ejercicio switch

--- TareaSemana3/Program.cs
+++ TareaSemana3/Program.cs
@@ -4,26 +4,28 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("ingresa el idioma en que deseas el mensaje");
-        string idioma = Console.ReadLine();
-        string mensaje; 
+        Console.WriteLine("ingresa tu calificacion por favor");
+        int calificacion = Convert.ToInt32(Console.ReadLine().ToLower());
+        string resultado;
 
-
-        switch (idioma)
+        switch (calificacion)
         {
-            case "ingles":
-                mensaje = "Welcome to my program";
+            case int b when b >= 90 && b <= 100:
+                resultado = "Sobresaliente. FELICIDADES!!!!";
                 break;
-            case "espanol":
-                mensaje = "Bienvenido a mi programa";
+            case int b when b >= 80 && b <= 89:
+                resultado = "Notable. Tu calificacion es promedio";
                 break;
-            case "frances":
-                mensaje = "Bienvenue dans mon programme";
+            case int b when b >= 70 && b <= 79:
+                resultado = "Aprobada. Esfuerzate, se que puedes lograr mas";
+                break;
+            case int b when b >= 60 && b <= 69:
+                resultado = "Reprobado. Que lastima que no lo lograste";
                 break;
             default:
-                Console.WriteLine("El idioma solicitado no es valido :(");
+                Console.WriteLine("El numero ingresado no es valido como calificacion :(");
                 return;
         }
-        Console.WriteLine($"Tu mensaje es: " + mensaje);
+        Console.WriteLine($"Tu resultado es: " + resultado);
     }
 }
