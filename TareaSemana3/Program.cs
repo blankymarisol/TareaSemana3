f833c60ca944f7c0bd6c3c68de823698b1a24d15
blanksss <marrophph@gmail.com>
2024-02-23 21:25:24 -0600

    Agregar archivos de proyecto.

---
+++ TareaSemana3/Program.cs
@@ -0,0 +1,29 @@
+﻿int num1, num2, num3;
+
+try
+{
+
+    Console.WriteLine("Por favor ingresa un numero");
+    num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Por favor ingresa un numero");
+    num2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Por favor ingresa un numero");
+    num3 = int.Parse(Console.ReadLine());
+
+    if (num1 >= num2 && num1 >= num3)
+    {
+        Console.WriteLine("El numero mayor es: " + num1);
+    }
+    else if (num2 >= num1 && num2 >= num3)
+    {
+        Console.WriteLine("El numero mayor es: " + num2);
+    }
+    else
+    {
+        Console.WriteLine("El numero mayor es: " + num3);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("No ingresaste lo que se te solicito :(" + ex.Message);
+}
