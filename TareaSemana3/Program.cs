1e4ef9eeba7dea6f137d731b1ad8caf4e0ce0eab
blanksss <marrophph@gmail.com>
2024-02-23 22:01:15 -0600

    primer ejercicio switch

--- TareaSemana3/Program.cs
+++ TareaSemana3/Program.cs
@@ -1,36 +1,34 @@
-﻿int opcion;
-double triangulo, cuadrado, circulo, basse, altura, lado, radio;
+﻿using System;
 
-try
+class Program
 {
-    Console.WriteLine("Elige una opcion: 1. Triangulo, 2. Cuadrado, 3. Circulo");
-    opcion = int.Parse(Console.ReadLine());
+    static void Main()
+    {
+        Console.WriteLine("ingrese un numero 1-5 en letra");
+        string letra = Console.ReadLine().ToLower();
+        int numero;
 
-    if (opcion == 1)
-    {
-        Console.WriteLine("Ingrese la base del triangulo");
-        basse = int.Parse(Console.ReadLine());
-        Console.WriteLine("Ingrese la altura del triangulo");
-        altura = int.Parse(Console.ReadLine());
-        triangulo = (basse * altura) / 2;
-        Console.WriteLine("El area del triangulo es: " + triangulo);
+        switch (letra)
+        {
+            case "uno":
+                numero = 1;
+                break;
+            case "dos":
+                numero = 2;
+                break;
+            case "tres":
+                numero = 3;
+                break;
+            case "cuatro":
+                numero = 4;
+                break;
+            case "cinco":
+                numero = 5;
+                break;
+            default:
+                Console.WriteLine("No ingresas el numero correcto :(");
+                return;
+        }
+        Console.WriteLine($"El numero en digito es: " + numero);
     }
-    else if (opcion == 2)
-    {
-        Console.WriteLine("Ingrese la medida del lado del cuadrado");
-        lado = int.Parse(Console.ReadLine());
-        cuadrado = lado * lado;
-        Console.WriteLine("El area del cuadrado es: " + cuadrado);
-    }
-    else
-    {
-        Console.WriteLine("Ingrese el radio del circulo");
-        radio = int.Parse(Console.ReadLine());
-        circulo = (radio * radio) * 3.1416;
-        Console.WriteLine("El area del circulo es: " + circulo);
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine("No ingresaste lo que se te solicito :(" + ex.Message);
 }
